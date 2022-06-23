c54e0def48f5737453310c7b1e83eefa32486960
ang_pop_os <aakashnareshgupta@gmail.com>
2022-06-23 11:27:00 +0530

    created buffered adder for synthesis

--- adder2Stage/adderBuf.v
+++ adder2Stage/adderBuf.v
@@ -1,11 +1,20 @@
-`include "adderGenerator.v"
+module adderGenerator #(parameter WIDTH = 16) (
+    input [WIDTH -1 : 0] in_a, 
+    input [WIDTH -1 : 0] in_b, 
+    input in_carry,
+    output [WIDTH - 1 : 0] sum,
+    output out_carry 
+);
+
+    assign {out_carry, sum} = in_a + in_b;
+endmodule
 
 module adderBuf (
     input clock,
     input reset,
     input [15:0] in_a, 
     input [15:0] in_b,
-    output [15:0] out_sum
+    output [16:0] out_sum
 );
 
 reg [15:0] reg_a;
@@ -17,13 +26,22 @@ wire [15:0] sum0;
 wire carry0;
 
 always @(posedge clock) begin
+    if(reset) begin
+        reg_a <= 16'b0;
+        reg_b <= 16'b0;
+        reg_sum <= 16'b0;
+        reg_carry <= 1'b0;
 
+    end
+else begin
     reg_a <= in_a;
     reg_b <= in_b;
     reg_sum <= sum0;    
     reg_carry <= carry0;
 end
+end
 adderGenerator adder(reg_a, reg_b, 1'b0, sum0, carry0);
-assign out_sum = reg_sum;
+assign out_sum ={reg_carry, reg_sum};
+// assign out_carry = reg_carry;
     
 endmodule
---
+++ adder2Stage/adderBuf_tb.v
@@ -0,0 +1,58 @@
+`timescale  1ns / 1ps
+
+module tb_adderBuf;
+
+reg clock;
+reg reset;
+reg[15:0] in_a;
+reg[15:0] in_b;
+
+wire [16:0] out_sum;
+
+adderBuf uut (
+    .clock(clock),
+    .reset(reset),
+    .in_a(in_a),
+    .in_b(in_b),
+    .out_sum(out_sum));
+
+parameter PERIOD = 10;
+
+initial begin
+    $dumpfile("db_tb_adderBuf.vcd");
+    $dumpvars(0, tb_adderBuf);
+    $monitor(clock, reset,in_a, in_b, out_sum);
+    clock = 1'b0;
+    reset = 1'b0;
+    in_a = 16'b0;
+    in_b = 16'b0;
+    #(PERIOD/2);
+    forever
+        #(PERIOD/2) clock = ~clock;
+
+    // #1000;
+    // $finish();
+end
+
+initial begin
+    #20;
+    #(PERIOD);
+    in_a = 16'd199;
+    in_b = 16'd1;    
+    
+    #(PERIOD);
+    in_a = 16'd199;
+    in_b = 16'd100;
+
+    #(PERIOD);
+    in_a = 16'd1990;
+    in_b = 16'd183;
+    #(PERIOD);
+    #(PERIOD);
+    #(PERIOD);
+    $finish();
+
+end
+
+
+endmodule
